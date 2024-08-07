dab0b07c58a1a05aae6fabb2d877f07f53f91954
tianma630 <tianma630@foxmail.com>
2024-08-07 12:09:12 +0800

    legal insights filter clearall

--- wp-content/plugins/xtech-yingke/assets/scripts/filter.js
+++ wp-content/plugins/xtech-yingke/assets/scripts/filter.js
@@ -2,22 +2,24 @@
 (function ($) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  
+
   var paged = 1;
-  var offset = $('.xtech-news-col').length;
-  
-  const formValues = $('.xtech-news-filter').serializeArray().reduce(function (obj, item) {
-        if (!obj[item.name]) {
-            obj[item.name] = item.name.includes('[]') ? [item.value] : item.value;
-        } else {
-            obj[item.name].push(item.value);
-        }
-        return obj;
-  }, {});
-  
+  var offset = $(".xtech-news-col").length;
+
+  const formValues = $(".xtech-news-filter")
+    .serializeArray()
+    .reduce(function (obj, item) {
+      if (!obj[item.name]) {
+        obj[item.name] = item.name.includes("[]") ? [item.value] : item.value;
+      } else {
+        obj[item.name].push(item.value);
+      }
+      return obj;
+    }, {});
+
   function fetchPosts() {
     $.ajax({
-      url: wpAjax.ajaxUrl+'morePosts',
+      url: wpAjax.ajaxUrl + "morePosts",
       data: {
         ...formValues,
         paged,
@@ -26,10 +28,10 @@
       type: "get",
       contentType: "application/javascript",
       beforeSend: function (xhr) {
-        $(".load-more-news .text").text('Loading...'); // changing the button label
+        $(".load-more-news .text").text("Loading..."); // changing the button label
       },
       success: function (result) {
-        $(".load-more-news .text").text('Load more'); // changing the button label
+        $(".load-more-news .text").text("Load more"); // changing the button label
         if ($.trim(result) != "") {
           $("#no-result").hide();
           $(".load-more-news").show();
@@ -37,11 +39,10 @@
           offset = $(".xtech-news-col").length;
         } else {
           if (paged === 1) {
-              $("#no-result").show();
+            $("#no-result").show();
           }
           $(".load-more-news").hide();
         }
-
       },
       error: function (result) {
         console.warn(result);
@@ -51,99 +52,141 @@
 
   $(".load-more-news").on("click", function (e) {
     e.preventDefault();
-    
+
     paged++;
-    
+
     fetchPosts(offset);
   });
 
   const params = new URL(location.href).searchParams;
-  const s = params.get('s');
-  const postTag = params.get('post_tag');
+  const s = params.get("s");
+  const postTag = params.get("post_tag");
   var searchPage = 1;
-  
+
   $(".load-more-search").on("click", function (e) {
-      e.preventDefault();
-      $.ajax({
-          url: wpAjax.ajaxUrl + 'search',
-          data: {
-            s,
-            paged: searchPage,
-            post_tag: postTag,
-          },
-          type: "get",
-          contentType: "application/javascript",
-          beforeSend: function (xhr) {
-            $(".load-more-search .text").text('Loading...'); // changing the button label
-          },
-          success: function (result) {
-              $(".load-more-search .text").text('Load more');
-              
-              if ($.trim(result) === "") {
-                  $("#no-result").show();
-                  $(".load-more-search").hide();
-                  return;
-              }
-              
-              $("#no-result").hide();
-              
-              $(".page-content").append(result);
-              searchPage++;
-          },
-          error: function (result) {
-            console.warn(result);
-          },
-      });
+    e.preventDefault();
+    $.ajax({
+      url: wpAjax.ajaxUrl + "search",
+      data: {
+        s,
+        paged: searchPage,
+        post_tag: postTag,
+      },
+      type: "get",
+      contentType: "application/javascript",
+      beforeSend: function (xhr) {
+        $(".load-more-search .text").text("Loading..."); // changing the button label
+      },
+      success: function (result) {
+        $(".load-more-search .text").text("Load more");
+
+        if ($.trim(result) === "") {
+          $("#no-result").show();
+          $(".load-more-search").hide();
+          return;
+        }
+
+        $("#no-result").hide();
+
+        $(".page-content").append(result);
+        searchPage++;
+      },
+      error: function (result) {
+        console.warn(result);
+      },
+    });
   });
   $(document).ready(() => {
-      if ($(".load-more-search")) {
-          $(".load-more-search").click();
-      }
+    if ($(".load-more-search")) {
+      $(".load-more-search").click();
+    }
   });
-  
-  $(".xtech-news-filter-options .form-check:first-child .form-check-input").on("click", function(e) {
-      if (!$(this).is(':checked')) {
-          e.preventDefault();
+
+  $(".xtech-news-filter-options .form-check:first-child .form-check-input").on(
+    "click",
+    function (e) {
+      if (!$(this).is(":checked")) {
+        e.preventDefault();
       }
-  });
-  
-  $(".xtech-news-filter-options-section input[type=checkbox]").on("change", function (e) {
-      const optionType = $(this).parents(".xtech-news-filter-options-section").data("option");
+    }
+  );
+
+  $(".xtech-news-filter-options-section input[type=checkbox]").on(
+    "change",
+    function (e) {
+      const optionType = $(this)
+        .parents(".xtech-news-filter-options-section")
+        .data("option");
       const optionAll = optionType + "-all";
       const optionCounter = optionType + "-counter";
-      const count = $(this).parents(".xtech-news-filter-options-section").find('input:not(#' + optionAll + '):checked').length;
-      
-      if ($(this).attr('id') !== optionAll) {
-          $('#' + optionAll).prop('checked', false);
-          $('#' + optionCounter).text(count)
+      const count = $(this)
+        .parents(".xtech-news-filter-options-section")
+        .find("input:not(#" + optionAll + "):checked").length;
+
+      if ($(this).attr("id") !== optionAll) {
+        $("#" + optionAll).prop("checked", false);
+        $("#" + optionCounter).text(count);
       } else {
-          $(this).parents(".xtech-news-filter-options-section").find("input[type=checkbox]:not(" + '#' + optionAll + ")").prop('checked', false);
-          $('#' + optionCounter).text('all')
+        $(this)
+          .parents(".xtech-news-filter-options-section")
+          .find("input[type=checkbox]:not(" + "#" + optionAll + ")")
+          .prop("checked", false);
+        $("#" + optionCounter).text("all");
       }
-      
-      formValues[optionType + '[]'] = $('input[name="' + optionType + '[]"]:checked').map(function () {
+
+      formValues[optionType + "[]"] = $(
+        'input[name="' + optionType + '[]"]:checked'
+      )
+        .map(function () {
           return this.value;
-      }).get();
-      
-      $("#news-response .xtech-news-row").html('');
+        })
+        .get();
+
+      $("#news-response .xtech-news-row").html("");
       paged = 1;
       offset = 0;
-      
+
       fetchPosts();
-  });
-  
+    }
+  );
+
   $(".xtech-news-filter #search").keypress(function (e) {
     if (e.which == 13) {
       formValues.s = $(this).val();
-      
-      $("#news-response .xtech-news-row").html('');
+
+      $("#news-response .xtech-news-row").html("");
       paged = 1;
       offset = 0;
-      
+
       fetchPosts();
-      
+
       return false;
     }
-  })
-  
+  });
+
+  $(".xtech-news-filter-clearall").on("click", (e) => {
+    e.preventDefault();
+
+    $(".xtech-news-filter-options-section").each((i, item) => {
+      const optionType = $(item).data("option");
+      delete formValues[optionType + "[]"];
+      $("#" + optionType + "-counter").text("all");
+
+      $(item)
+        .find("form-check-input")
+        .each((j, check) => {
+          if (j === 0) {
+            $(check).prop("checked", true);
+          } else {
+            $(check).prop("checked", true);
+          }
+        });
+    });
+
+    $("#news-response .xtech-news-row").html("");
+    paged = 1;
+    offset = 0;
+
+    fetchPosts();
+  });
 })(jQuery);
--- wp-content/plugins/xtech-yingke/widgets/filter.php
+++ wp-content/plugins/xtech-yingke/widgets/filter.php
@@ -85,138 +85,140 @@ class Widget_Filter extends Widget_Base
   {
     $settings = $this->get_settings_for_display();
     $filterType = esc_attr($settings['filterType']);
-    
+
     $types = $_GET['types'] ?? [];
     $practices = $_GET['practices'] ?? [];
     $dates = $_GET['dates'] ?? [];
 
-    $newsTypes = get_terms([ 'taxonomy' => 'category', 'hide_empty' => false ]);
-    
-    $newsPractices = get_terms([ 'taxonomy' => 'practice', 'hide_empty' => false ]);
-    
+    $newsTypes = get_terms(['taxonomy' => 'category', 'hide_empty' => false]);
+
+    $newsPractices = get_terms(['taxonomy' => 'practice', 'hide_empty' => false]);
+
     $archives = $this->get_year_archive_array();
-    
-    $typeCount = count(array_intersect($types, get_terms([ 'taxonomy' => 'category', 'hide_empty' => false, 'fields' => 'slugs' ])));
-    
-    $practiceCount = count(array_intersect($practices, get_terms([ 'taxonomy' => 'practice', 'hide_empty' => false, 'fields' => 'slugs' ])));
-    
+
+    $typeCount = count(array_intersect($types, get_terms(['taxonomy' => 'category', 'hide_empty' => false, 'fields' => 'slugs'])));
+
+    $practiceCount = count(array_intersect($practices, get_terms(['taxonomy' => 'practice', 'hide_empty' => false, 'fields' => 'slugs'])));
+
     $dateCount = count(array_intersect($dates, $archives));
-    
+
     $className = "news-filter-item";
 ?>
     <form class="xtech-news-filter">
-        <div class="xtech-news-filter-search-form yingke-search-form">
-			<div class="input-wrapper">
-				<input type="text" name="s" id="search" placeholder="Search by keyword" value="<?php echo get_search_query(); ?>" />
-			</div>
-			<button class="submit-button"></button>
-		</div>
-		
-		<div class="xtech-news-filter-section">
-		    <div>
-		        <div class="xtech-news-filter-option-label">
-		            Filters:
-		        </div>
-		    </div>
-		    <div class="xtech-news-filter-options-section" data-option="types">
-		        <div class="xtech-news-filter-option-label">Type (<span id="types-counter"><?= $typeCount > 0 ? $typeCount : 'all' ?></span>)</div>
-		        <div class="xtech-news-filter-options-wrapper">
-    		        <div class="xtech-news-filter-options">
-    		            <div class="form-check">
-                            <input class="form-check-input" type="checkbox" id="types-all" <?= $typeCount === 0 ? 'checked' : '' ?>>
-                            <label class="form-check-label" for="types-all">
-                                All Types
-                            </label>
-                        </div>
-    		            <?php foreach ($newsTypes as $cat): ?>
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" name="types[]" value="<?= $cat->slug ?>" id="<?= $cat->slug ?>" <?php echo in_array($cat->slug, $types) ? 'checked' : '' ?>>
-                                <label class="form-check-label" for="<?= $cat->slug ?>">
-                                    <?= $cat->name ?>
-                                </label>
-                            </div>
-                        <?php endforeach ?>
-    		        </div>
-		        </div>
-		    </div>
-		    <div class="xtech-news-filter-options-section" data-option="practices">
-		        <div class="xtech-news-filter-option-label">Practice (<span id="practices-counter"><?= $practiceCount > 0 ? $practiceCount : 'all' ?></span>)</div>
-		        <div class="xtech-news-filter-options-wrapper">
-    		        <div class="xtech-news-filter-options">
-    		            <div class="form-check">
-                            <input class="form-check-input" type="checkbox" id="practices-all" <?= $practiceCount === 0 ? 'checked' : '' ?>>
-                            <label class="form-check-label" for="practices-all">
-                                All Practices
-                            </label>
-                        </div>
-    		            <?php foreach ($newsPractices as $practice): ?>
-                            <div class="form-check">
-                              <input class="form-check-input" type="checkbox" name="practices[]" value="<?= $practice->slug ?>" id="<?= $practice->slug ?>" <?php echo in_array($practice->slug, $practices) ? 'checked' : '' ?>>
-                              <label class="form-check-label" for="<?= $practice->slug ?>">
-                                <?= $practice->name ?>
-                              </label>
-                            </div>
-                        <?php endforeach ?>
-    		        </div>
-		        </div>
-		    </div>
-		    <div class="xtech-news-filter-options-section" data-option="dates">
-		        <div class="xtech-news-filter-option-label">Date (<span id="dates-counter"><?= $dateCount > 0 ? $dateCount : 'all' ?></span>)</div>
-		        <div class="xtech-news-filter-options-wrapper">
-    		        <div class="xtech-news-filter-options">
-    		            <div class="form-check">
-                            <input class="form-check-input" type="checkbox" id="dates-all" <?= $dateCount === 0 ? 'checked' : '' ?>>
-                            <label class="form-check-label" for="dates-all">
-                                All Dates
-                            </label>
-                        </div>
-    		            <?php foreach ($archives as $index => $archive): ?>
-                            <div class="form-check">
-                              <input class="form-check-input" type="checkbox" name="dates[]" value="<?= $archive ?>" id="date-<?= $index ?>" <?php echo in_array($archive, $dates) ? 'checked' : '' ?>>
-                              <label class="form-check-label" for="date-<?= $index ?>">
-                                <?= $archive ?>
-                              </label>
-                            </div>
-                        <?php endforeach ?>
-    		        </div>
-		        </div>
-		    </div>
-		</div>
+      <div class="xtech-news-filter-search-form yingke-search-form">
+        <div class="input-wrapper">
+          <input type="text" name="s" id="search" placeholder="Search by keyword" value="<?php echo get_search_query(); ?>" />
+        </div>
+        <button class="submit-button"></button>
+      </div>
+
+      <div class="xtech-news-filter-section">
+        <div>
+          <div class="xtech-news-filter-option-label">
+            Filters:
+          </div>
+        </div>
+        <div class="xtech-news-filter-options-section" data-option="types">
+          <div class="xtech-news-filter-option-label">Type (<span id="types-counter"><?= $typeCount > 0 ? $typeCount : 'all' ?></span>)</div>
+          <div class="xtech-news-filter-options-wrapper">
+            <div class="xtech-news-filter-options">
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="types-all" <?= $typeCount === 0 ? 'checked' : '' ?>>
+                <label class="form-check-label" for="types-all">
+                  All Types
+                </label>
+              </div>
+              <?php foreach ($newsTypes as $cat) : ?>
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" name="types[]" value="<?= $cat->slug ?>" id="<?= $cat->slug ?>" <?php echo in_array($cat->slug, $types) ? 'checked' : '' ?>>
+                  <label class="form-check-label" for="<?= $cat->slug ?>">
+                    <?= $cat->name ?>
+                  </label>
+                </div>
+              <?php endforeach ?>
+            </div>
+          </div>
+        </div>
+        <div class="xtech-news-filter-options-section" data-option="practices">
+          <div class="xtech-news-filter-option-label">Practice (<span id="practices-counter"><?= $practiceCount > 0 ? $practiceCount : 'all' ?></span>)</div>
+          <div class="xtech-news-filter-options-wrapper">
+            <div class="xtech-news-filter-options">
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="practices-all" <?= $practiceCount === 0 ? 'checked' : '' ?>>
+                <label class="form-check-label" for="practices-all">
+                  All Practices
+                </label>
+              </div>
+              <?php foreach ($newsPractices as $practice) : ?>
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" name="practices[]" value="<?= $practice->slug ?>" id="<?= $practice->slug ?>" <?php echo in_array($practice->slug, $practices) ? 'checked' : '' ?>>
+                  <label class="form-check-label" for="<?= $practice->slug ?>">
+                    <?= $practice->name ?>
+                  </label>
+                </div>
+              <?php endforeach ?>
+            </div>
+          </div>
+        </div>
+        <div class="xtech-news-filter-options-section" data-option="dates">
+          <div class="xtech-news-filter-option-label">Date (<span id="dates-counter"><?= $dateCount > 0 ? $dateCount : 'all' ?></span>)</div>
+          <div class="xtech-news-filter-options-wrapper">
+            <div class="xtech-news-filter-options">
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="dates-all" <?= $dateCount === 0 ? 'checked' : '' ?>>
+                <label class="form-check-label" for="dates-all">
+                  All Dates
+                </label>
+              </div>
+              <?php foreach ($archives as $index => $archive) : ?>
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" name="dates[]" value="<?= $archive ?>" id="date-<?= $index ?>" <?php echo in_array($archive, $dates) ? 'checked' : '' ?>>
+                  <label class="form-check-label" for="date-<?= $index ?>">
+                    <?= $archive ?>
+                  </label>
+                </div>
+              <?php endforeach ?>
+            </div>
+          </div>
+        </div>
+        <div class="xtech-news-filter-clearall"><a id="clear-filter-value" href="#"><?php esc_html_e('Clear All'); ?></a></div>
+      </div>
     </form>
 
 <?php
 
   }
-  
-    function get_year_archive_array() {
-      $years = array();
-      $years_args = array(
-        'type' => 'monthly',
-        'format' => 'custom',
-        'before' => '',
-        'after' => '|',
-        'echo' => false,
-        'order' => 'DESC'
-      );
-    
-      // Get Years
-      $years_content = wp_get_archives($years_args);
-      if (!empty($years_content) ) {
-        $years_arr = explode('|', $years_content);
-        $years_arr = array_filter($years_arr, function($item) {
-          return trim($item) !== '';
-        }); // Remove empty whitespace item from array
-    
-        foreach($years_arr as $year_item) {
-          $year_row = trim($year_item);
-          preg_match('/href=["\']?([^"\'>]+)["\']>(.+)<\/a>/', $year_row, $year_vars);
-    
-          if (!empty($year_vars)) {
-            $years[] = $year_vars[2];
-          }
+
+  function get_year_archive_array()
+  {
+    $years = array();
+    $years_args = array(
+      'type' => 'monthly',
+      'format' => 'custom',
+      'before' => '',
+      'after' => '|',
+      'echo' => false,
+      'order' => 'DESC'
+    );
+
+    // Get Years
+    $years_content = wp_get_archives($years_args);
+    if (!empty($years_content)) {
+      $years_arr = explode('|', $years_content);
+      $years_arr = array_filter($years_arr, function ($item) {
+        return trim($item) !== '';
+      }); // Remove empty whitespace item from array
+
+      foreach ($years_arr as $year_item) {
+        $year_row = trim($year_item);
+        preg_match('/href=["\']?([^"\'>]+)["\']>(.+)<\/a>/', $year_row, $year_vars);
+
+        if (!empty($year_vars)) {
+          $years[] = $year_vars[2];
         }
       }
-    
-      return $years;
     }
+
+    return $years;
+  }
 }
